e56d41dc2f7fd274a5346f1f4b59a3d50b31c5a3
shaikat17 <shaikatpal56@gmail.com>
2024-07-01 19:06:58 +0600

    Working with logo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 
-import { icons } from '../../constants'
+import { icons } from '@/constants'
 
 const TabIcon = ({icon, color, name, focused}) => {
   return (
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
-import { ScrollView, StatusBar, Text, View } from 'react-native'
+import { Image, ScrollView, StatusBar, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '@/constants'
 
 const index = () => {
   return (
@@ -9,7 +11,12 @@ const index = () => {
       <ScrollView contentContainerStyle={{
         height: '100%'
       }}>
-
+        <View className="h-full w-full items-center justify-center">
+          <Image 
+          source={images.logo}
+          className='w-[130px h-[84px'
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   )

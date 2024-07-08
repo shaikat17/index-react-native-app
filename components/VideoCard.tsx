055c8054c4a41370d17286d0110a633a2e01e6f6
shaikat17 <shaikatpal56@gmail.com>
2024-07-09 01:09:02 +0600

    working on video card

---
+++ components/VideoCard.tsx
@@ -0,0 +1,54 @@
+import { View, Text, Image, Pressable } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '@/constants'
+
+const VideoCard = ({ video: { title, thumbnail, video, creator: {username, avatar}}}) => {
+
+    const [play, setPlay] = useState(false)
+  return (
+    <View className='flex-col items-center px-4 mb-14'>
+        <View className='flex-row gap-3 items-start'>
+<View className='justify-center items-center flex-row flex-1'>
+<View className='w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5'>
+<Image source={{uri: avatar}}
+className='w-full h-full rounded-lg'
+resizeMode='cover' />
+</View>
+<View className='justify-center flex-1 ml-3 gay-y-1'>
+<Text className='text-white font-psemibold text-sm' numberOfLines={1}>
+    {title}
+</Text>
+<Text className='text-xs text-gray-100 font-pregulare' numberOfLines={1}>
+    {username}
+</Text>
+</View>
+</View>
+<View className='pt-2'>
+    <Image
+    source={icons.menu}
+    className='w-5 h-5'
+    resizeMode='contain' />
+</View>
+        </View>
+{play ? (
+    <Text className='text-white'>Playing</Text>
+) : (
+    <Pressable
+     className='w-full h-60 mt-3 relative justify-center items-center'
+      onPress={() => console.log('Hello')}
+     >
+        <Image
+        source={{ uri: thumbnail}}
+        className='w-full h-full rounded-lg mt-3'
+        resizeMode='cover' />
+        <Image
+        source={icons.play}
+        className='w-12 h-12 absolute'
+        resizeMode='contain' />
+    </Pressable>
+)}
+    </View>
+  )
+}
+
+export default VideoCard

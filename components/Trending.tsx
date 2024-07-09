edd8e3d8a4e43334aef22ce36b74492c1d12cef1
shaikat17 <shaikatpal56@gmail.com>
2024-07-09 15:44:11 +0600

    Trending videos added with animation.

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,16 +1,89 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+  Pressable,
+  ImageBackground,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import { icons } from "@/constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+  console.log('active item: ',activeItem.$id, 'item: ',item.$id);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <Pressable
+          className="relative justify-center items-center"
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="absolute w-12 h-12"
+            resizeMode="contain"
+          />
+        </Pressable>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
   return (
     <FlatList
-    data={posts}
-    keyExtractor={(item) => item.id}
-    renderItem={({ item }) => (
-        <Text className='text-white'>{item.id}</Text>
-    )}
-    horizontal />
-  )
-}
+      data={posts}
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => (
+        <TrendingItem activeItem={activeItem} item={item} />
+      )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{ x: 170 }}
+      horizontal
+    />
+  );
+};
 
-export default Trending
+export default Trending;
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,54 +1,72 @@
-import { View, Text, Image, Pressable } from 'react-native'
-import React, { useState } from 'react'
-import { icons } from '@/constants'
+import { View, Text, Image, Pressable } from "react-native";
+import React, { useState } from "react";
+import { icons } from "@/constants";
 
-const VideoCard = ({ video: { title, thumbnail, video, creator: {username, avatar}}}) => {
-
-    const [play, setPlay] = useState(false)
+const VideoCard = ({
+  video: {
+    title,
+    thumbnail,
+    video,
+    creator: { username, avatar },
+  },
+}) => {
+  const [play, setPlay] = useState(false);
   return (
-    <View className='flex-col items-center px-4 mb-14'>
-        <View className='flex-row gap-3 items-start'>
-<View className='justify-center items-center flex-row flex-1'>
-<View className='w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5'>
-<Image source={{uri: avatar}}
-className='w-full h-full rounded-lg'
-resizeMode='cover' />
-</View>
-<View className='justify-center flex-1 ml-3 gay-y-1'>
-<Text className='text-white font-psemibold text-sm' numberOfLines={1}>
-    {title}
-</Text>
-<Text className='text-xs text-gray-100 font-pregulare' numberOfLines={1}>
-    {username}
-</Text>
-</View>
-</View>
-<View className='pt-2'>
-    <Image
-    source={icons.menu}
-    className='w-5 h-5'
-    resizeMode='contain' />
-</View>
+    <View className="flex-col items-center px-4 mb-14">
+      <View className="flex-row gap-3 items-start">
+        <View className="justify-center items-center flex-row flex-1">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+            <Image
+              source={{ uri: avatar }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
+            />
+          </View>
+          <View className="justify-center flex-1 ml-3 gay-y-1">
+            <Text
+              className="text-white font-psemibold text-sm"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            <Text
+              className="text-xs text-gray-100 font-pregulare"
+              numberOfLines={1}
+            >
+              {username}
+            </Text>
+          </View>
         </View>
-{play ? (
-    <Text className='text-white'>Playing</Text>
-) : (
-    <Pressable
-     className='w-full h-60 mt-3 relative justify-center items-center'
-      onPress={() => console.log('Hello')}
-     >
-        <Image
-        source={{ uri: thumbnail}}
-        className='w-full h-full rounded-lg mt-3'
-        resizeMode='cover' />
-        <Image
-        source={icons.play}
-        className='w-12 h-12 absolute'
-        resizeMode='contain' />
-    </Pressable>
-)}
+        <View className="pt-2">
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+        </View>
+      </View>
+      {play ? (
+        <View>
+          <Text className="text-white">Playing</Text>
+        </View>
+      ) : (
+        <View className="w-full h-60">
+          <Pressable
+            android_ripple={{ color: 'rgba(255, 255, 255, 0.5)', foreground: true, borderless: false }}
+            className="w-full h-full mt-3 relative justify-center rounded-lg items-center overflow-hidden"
+            onPress={() => setPlay(true)}
+          >
+            <Image
+              source={{ uri: thumbnail }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
+            />
+            <Image
+              source={icons.play}
+              className="w-12 h-12 absolute"
+              resizeMode="contain"
+            />
+          </Pressable>
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
